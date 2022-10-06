d98ff65d3c37b2ee15d8607336274e4876fa1d5e
SanketBezalwar <sanketbezalwar45@gmail.com>
2022-10-06 14:24:09 +0530

    Employee Attendance Check

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -1,10 +1,22 @@
 package Employee_Wage_Computation_Day3;
 
+import java.util.Random;
+
 public class Employee_Wage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
 		System.out.println();
+
+		// Employee attendance check
+		Random inp = new Random();
+		int isPresent = inp.nextInt(2);
+		
+		System.out.println("Attendance : ");
+		if (isPresent == 0)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
