8e63d42190a4229840172402b74aca1c5b05b9f2
SanketBezalwar <sanketbezalwar45@gmail.com>
2022-10-06 14:54:36 +0530

    Calculated daily employee wage

--- Employee_Wage.java
+++ Employee_Wage.java
@@ -3,20 +3,42 @@ package Employee_Wage_Computation_Day3;
 import java.util.Random;
 
 public class Employee_Wage {
+	static int wage_Per_Hour = 20;
+	static int full_Day_Hour = 8;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
 		System.out.println();
 
-		// Employee attendance check
+		Employee_Wage emp = new Employee_Wage();
+
+		emp.employeeAttendance();
+		emp.dailyEmployeeWage();
+
+	}
+
+	// employee attendance check
+	public void employeeAttendance() {
 		Random inp = new Random();
 		int isPresent = inp.nextInt(2);
-		
+
 		System.out.println("Attendance : ");
 		if (isPresent == 0)
 			System.out.println("Employee is Present");
 		else
 			System.out.println("Employee is Absent");
+		System.out.println();
+	}
+
+	// calculate employee wage
+	public void dailyEmployeeWage() {
+		int dailyWage1;
+
+		dailyWage1 = full_Day_Hour * wage_Per_Hour;
+		System.out.println("Calculate Daily Employee Wage : ");
+		System.out.println("Employee is Full Time");
+		System.out.println("Daily wage of Employee is " + dailyWage1 + " Rs ");
+		System.out.println();
 	}
 
 }
